6e04f4da857d18195dd56d0ca881babbfce7a402
nishan-jq <Nishan@teamtitans.chat>
2025-09-12 16:58:00 +0530

    add pin

--- src/service/TableDataService.ts
+++ src/service/TableDataService.ts
@@ -38,8 +38,15 @@ export const getTrashdata = async () => {
   }
 }
 
-
-
+export const getPindata = async () => {
+  try {
+    const response = await apiClient.get('/api/password/state/pin')
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+// -------------------------------------------------------------------------
 
 
 // POST API for password Generator
